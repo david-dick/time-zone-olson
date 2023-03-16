17c5d81dd7817c78f5a91e6587d9648f90baee24
David Dick <ddick@cpan.org>
2023-03-16 17:06:38 +1100

    Code cleanup

--- date.cs
+++ date.cs
@@ -15,12 +15,7 @@ class Program {
 		DateTime timeUtc = DateTime.ParseExact(args[0], "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
 		TimeZoneInfo zone = TimeZoneInfo.FindSystemTimeZoneById(args[1]);
 		DateTime time = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, zone);
-		string zoneName;
-		if (zone.StandardName == TimeZoneInfo.Utc.StandardName) {
-			zoneName = "UTC";
-		} else {
-			zoneName = zone.IsDaylightSavingTime(time) ?  zone.DaylightName : zone.StandardName;
-		}
+		string zoneName = zone.IsDaylightSavingTime(time) ?  zone.DaylightName : zone.StandardName;
 		Console.WriteLine("{0} {1}", time.ToString("yyyy/MM/dd HH:mm:ss"), zoneName);
 	}
 }
